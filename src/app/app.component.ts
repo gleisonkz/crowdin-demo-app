960ce4a82bf32b3347cfbc7f11705e9b0eab9b8e
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-07-02 19:40:53 -0300

    :sparkles: feat: add transloco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { TranslocoService } from '@ngneat/transloco';
 
 @Component({
   selector: 'cwd-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'crowdin-demo-app';
+  siteLanguage = 'English';
+
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'es', label: 'Español' },
+    { code: 'pt-BR', label: 'Português' },
+  ];
+
+  constructor(private service: TranslocoService) {}
+
+  changeSiteLanguage(language: string): void {
+    this.service.setActiveLang(language);
+    this.siteLanguage = this.languageList.find(
+      (f) => f.code === language
+    )?.label!;
+  }
 }
